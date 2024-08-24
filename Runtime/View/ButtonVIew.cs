115c35929e542b46a3657983380fdb9537ede43b
Lalo <laurencioberro@hotmail.com>
2024-08-24 18:56:20 -0300

    [A] Added ButtonView

---
+++ Runtime/View/ButtonVIew.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using MVVM.Core;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace View
+{
+    public abstract class ButtonView : MonoBehaviour
+    {
+        [Header("References")]
+        [SerializeField] protected Button _button;
+        [SerializeField] private EventViewModelSO _eventViewModelSo;
+
+        private IEventViewModel _eventViewModel;
+
+        private void Awake()
+        {
+            _eventViewModel = _eventViewModelSo.GetEventViewModel();
+            
+            Subscribe(_button);
+        }
+
+        protected abstract void Subscribe(Button button);
+
+        protected void Raise()
+        {
+            _eventViewModel.RaiseEvent();
+        }
+
+        protected abstract void Dispose(Button button);
+    }
+}
---
+++ Runtime/View/OnClickButtonView.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine.UI;
+
+namespace View
+{
+    public class OnClickButtonView : ButtonView
+    {
+        protected override void Subscribe(Button button)
+        {
+            button.onClick.AddListener(Raise);
+        }
+
+        protected override void Dispose(Button button)
+        {
+            button.onClick.RemoveListener(Raise);
+        }
+    }
+}
