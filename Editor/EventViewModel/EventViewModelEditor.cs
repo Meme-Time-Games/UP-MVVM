529cd3e41dcf6b9ac9f6766bbcbdc7249927b7d8
Lalo <laurencioberro@hotmail.com>
2024-08-24 20:20:00 -0300

    [R] Refactored EventViewModelEditor

--- Editor/EventViewModel/EventViewModelEditor.cs
+++ Editor/EventViewModel/EventViewModelEditor.cs
@@ -20,23 +20,11 @@ namespace MVVM.CoreEditor
 
             DrawRaiseEventIfIsPlaying(eventViewModel);
 
-            if (_allComponentReferences != null)
-                DrawReferenceComponents();
-
-            if (GUILayout.Button("Search references"))
-                DrawSearchReferenceInTheScene(eventViewModelSo);
+            if (GUILayout.Button("Open References Searcher"))
+                DrawSearchReferenceInTheScene();
         }
 
-        private void DrawReferenceComponents()
-        {
-            foreach (var component in _allComponentReferences)
-            {
-                if (GUILayout.Button(component.gameObject.name))
-                    Selection.activeGameObject = component.gameObject;
-            }
-        }
-
-        private static void DrawRaiseEventIfIsPlaying(IEventViewModel eventViewModel)
+        private void DrawRaiseEventIfIsPlaying(IEventViewModel eventViewModel)
         {
             if (!EditorApplication.isPlaying) 
                 return;
@@ -45,41 +33,9 @@ namespace MVVM.CoreEditor
                 eventViewModel.RaiseEvent();
         }
         
-        private void DrawSearchReferenceInTheScene(EventViewModelSO eventViewModelSo)
+        private void DrawSearchReferenceInTheScene()
         {
             EventViewModelReferencesEditorWindow.ShowWindow();   
-            // _allComponentReferences = GetAllReferences(eventViewModelSo);
-        }
-
-        private List<Component> GetAllReferences(EventViewModelSO eventViewModelSo)
-        {
-            List<Component> allReferencedObjects = new List<Component>();
-            List<GameObject> allGameObjects = new List<GameObject>();
-            allGameObjects.AddRange(FindObjectsOfType<GameObject>());
-
-            foreach (var go in allGameObjects)
-            {
-                Component[] components = go.GetComponents<Component>();
-
-                foreach (var component in components)
-                {
-                    SerializedObject so = new SerializedObject(component);
-                    SerializedProperty prop = so.GetIterator();
-
-                    while (prop.NextVisible(true))
-                    {
-                        if (prop.propertyType != SerializedPropertyType.ObjectReference ||
-                            prop.objectReferenceValue != eventViewModelSo)
-                            continue;
-
-                        allReferencedObjects.Add(component);
-                        Debug.Log($"Found reference to {eventViewModelSo.name} in component {component.GetType().Name} on GameObject {go.name}", go);
-                    }
-                }
-            }
-
-            Debug.Log($"Total references found: {allReferencedObjects.Count}");
-            return allReferencedObjects;
         }
     }
 }
