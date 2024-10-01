1db4d86014d1ba0b77b39ead1d8d4ee1d351d68c
Lalo <laurencioberro@hotmail.com>
2024-10-01 17:51:01 -0300

    [A] Added ReactiveVariableInstallSkipChecker

---
+++ Runtime/InstallSkipChecker/BoolReactiveVariableInstallSkipChecker.cs
@@ -0,0 +1,12 @@
+﻿namespace InstallSkipChecker
+{
+    public class BoolReactiveVariableInstallSkipChecker : ReactiveVariableInstallSkipChecker<bool>
+    {
+        protected override bool Compare(bool conditionValue, bool reactiveVariableValue)
+        {
+            bool areEquals = conditionValue == reactiveVariableValue;
+
+            return areEquals;
+        }
+    }
+}
---
+++ Runtime/InstallSkipChecker/ReactiveVariableInstallSkipChecker.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using DependencyInjector.Installers;
+using MVVM.Core;
+using UnityEngine;
+
+namespace InstallSkipChecker
+{
+    public abstract class ReactiveVariableInstallSkipChecker<TType> : MonoInstallSkipChecker
+    {
+        [Header("References")]
+        [SerializeField] private ReactiveVariableSO<TType> _reactiveVariable;
+        
+        [Header("Config")] 
+        [SerializeField] private TType _hasToSkipIfConditionIs;
+        
+        public override bool HasToSkip()
+        {
+            TType reactiveVariableValue = _reactiveVariable.GetReactiveVariable().Value;
+            bool result = Compare(_hasToSkipIfConditionIs, reactiveVariableValue);
+            return result;
+        }
+
+        protected abstract bool Compare(TType conditionValue, TType reactiveVariableValue);
+    }
+}
