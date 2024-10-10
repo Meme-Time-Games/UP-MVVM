bddddf44dbdc497f17f9088b73455230b4a31fc4
Lalo <laurencioberro@hotmail.com>
2024-10-10 16:49:27 -0300

    [A] Added ReactiveVariableController

---
+++ Runtime/Core/Installers/ReactiveVariableControllerInstaller.cs
@@ -0,0 +1,19 @@
+﻿using DependencyInjector.Installers;
+using MVVM.Core.InterfaceAdapters;
+using UnityEngine;
+
+namespace MVVM.Core.Installers
+{
+    public abstract class ReactiveVariableControllerInstaller<TType> : MonoInstaller<IController>
+    {
+        [Header("References")]
+        [SerializeField] private ReactiveVariableSO<TType> _reactiveVariableSo;
+
+        protected override IController GetData()
+        {
+            return GetController(_reactiveVariableSo.GetReactiveVariable());
+        }
+
+        protected abstract IController GetController(IReactiveVariable<TType> reactiveVariable);
+    }
+}
---
+++ Runtime/Core/InterfaceAdapters/EventViewModel/Controllers/ReactiveVariableController.cs
@@ -0,0 +1,28 @@
+﻿namespace MVVM.Core.InterfaceAdapters
+{
+    public abstract class ReactiveVariableController<TType> : IController
+    {
+        private readonly IReactiveVariable<TType> _reactiveVariable;
+
+        protected ReactiveVariableController(IReactiveVariable<TType> reactiveVariable)
+        {
+            _reactiveVariable = reactiveVariable;
+
+            _reactiveVariable.OnValueChanged += Execute;
+        }
+
+        public void Execute()
+        {
+            TType value = _reactiveVariable.Value;
+
+            ExecuteWithValue(value);
+        }
+
+        protected abstract void ExecuteWithValue(TType value);
+
+        public void Dispose()
+        {
+            _reactiveVariable.OnValueChanged -= Execute;
+        }
+    }
+}
