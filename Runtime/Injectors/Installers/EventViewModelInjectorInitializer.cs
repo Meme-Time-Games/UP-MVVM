2c22690630e66bd815f475c643925316e6f26fcd
Lalo <laurencioberro@hotmail.com>
2024-10-28 12:46:33 -0300

    [A] Added EventViewModelInjectorInitializer

---
+++ Runtime/Injectors/Installers/EventViewModelInjectorInitializer.cs
@@ -0,0 +1,26 @@
+﻿using DependencyInjector.Installers;
+using MVVM.Core;
+using UnityEngine;
+
+namespace MVVM
+{
+    public class EventViewModelInjectorInitializer : MonoBehaviour
+    {
+        [Header("References")] 
+        [SerializeField] private EventViewModelSO _eventViewModelSo; 
+        [SerializeField] private BaseMonoInjector _monoInjector;
+        
+        private IEventViewModel _eventViewModel;
+        
+        private void Awake()
+        {
+            _eventViewModel = _eventViewModelSo.GetEventViewModel();
+            _eventViewModel.OnEventRaised += Inject;
+        }
+
+        private void Inject()
+        {
+            _monoInjector.InjectAll();
+        }
+    }
+}
