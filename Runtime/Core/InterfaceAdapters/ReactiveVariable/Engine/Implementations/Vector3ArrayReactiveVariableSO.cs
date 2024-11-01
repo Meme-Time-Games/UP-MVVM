ced79671a68a33376f8e8d666a5f67a7e0e87845
Lalo <laurencioberro@hotmail.com>
2024-11-01 11:30:49 -0300

    [A] Added Array Vector3 reactive variable

---
+++ Runtime/Core/InterfaceAdapters/ReactiveVariable/Engine/Implementations/Vector3ArrayReactiveVariableSO.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace MVVM.Core
+{
+    [CreateAssetMenu(fileName = "Vector3Array-ReactiveVariableSO", menuName = "ScriptableObjects/ReactiveVariable/Array-Vector3")]
+    public class Vector3ArrayReactiveVariableSO : ReactiveVariableSO<Vector3[]>
+    {
+        
+    }
+}
