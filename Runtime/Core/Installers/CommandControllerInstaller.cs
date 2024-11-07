443a791cb81a189fae2860932faf46210eb79430
Lalo <laurencioberro@hotmail.com>
2024-11-07 13:00:15 -0300

    [A] Adde CommandController

---
+++ Runtime/Core/Installers/CommandControllerInstaller.cs
@@ -0,0 +1,21 @@
+﻿using Commands.Core;
+using DependencyInjector.Core;
+using MVVM.Core.Installers;
+
+namespace MVVM.Core.InterfaceAdapters
+{
+    public class CommandControllerInstaller : ControllerInstaller
+    {
+        [Inject] public ICommand _command;
+        
+        protected override void InstallServiceInContainer(IDIContainer diContainer, IController serviceInstance)
+        {
+            diContainer.RegisterAsSingle(diContainer);
+        }
+
+        protected override IController GetController(IEventViewModel eventBindingViewModel)
+        {
+            return new CommandController(eventBindingViewModel, _command);
+        }
+    }
+}
---
+++ Runtime/Core/InterfaceAdapters/EventViewModel/Controllers/CommandController.cs
@@ -0,0 +1,19 @@
+﻿using Commands.Core;
+
+namespace MVVM.Core.InterfaceAdapters
+{
+    public class CommandController : Controller
+    {
+        private readonly ICommand _command;
+        
+        public CommandController(IEventViewModel eventViewModel, ICommand command) : base(eventViewModel)
+        {
+            _command = command;
+        }
+
+        public override void Execute()
+        {
+            _command.Execute();
+        }
+    }
+}
