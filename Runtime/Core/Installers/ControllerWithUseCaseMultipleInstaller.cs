783f268edf53af80aac8e1e2d18486e0bd87ec19
Lalo <laurencioberro@hotmail.com>
2024-12-09 11:54:11 -0300

    [A] Added ControllerWithUseCase

---
+++ Runtime/Core/Installers/ControllerWithUseCaseMultipleInstaller.cs
@@ -0,0 +1,22 @@
+﻿using DependencyInjector.Core;
+using MVVM.Core.InterfaceAdapters;
+
+namespace MVVM.Core.Installers
+{
+    public abstract class ControllerWithUseCaseMultipleInstaller<TUseCase> : ControllerInstaller
+    {
+        [Inject] private TUseCase _useCase;
+        
+        protected override void InstallServiceInContainer(IDIContainer diContainer, IController serviceInstance)
+        {
+            diContainer.RegisterAsMultiple(serviceInstance);
+        }
+
+        protected override IController GetController(IEventViewModel eventBindingViewModel)
+        {
+            return GetControllerInstance(eventBindingViewModel, _useCase);
+        }
+
+        protected abstract IController GetControllerInstance(IEventViewModel eventBindingViewModel, TUseCase useCase);
+    }
+}
---
+++ Runtime/Core/InterfaceAdapters/EventViewModel/Controllers/ControllerWithUseCase.cs
@@ -0,0 +1,19 @@
+﻿namespace MVVM.Core.InterfaceAdapters
+{
+    public abstract class ControllerWithUseCase<TUseCase> : Controller
+    {
+        private readonly TUseCase _useCase;
+
+        protected ControllerWithUseCase(IEventViewModel eventViewModel, TUseCase useCase) : base(eventViewModel)
+        {
+            _useCase = useCase;
+        }
+
+        public override void Execute()
+        {
+            ExecuteUseCase(_useCase);
+        }
+
+        protected abstract void ExecuteUseCase(TUseCase useCase);
+    }
+}
