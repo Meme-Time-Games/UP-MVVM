9fced8334c7dc60f96e8a4ae52e317edf438ab66
Lalo <laurencioberro@hotmail.com>
2025-02-13 11:57:58 -0300

    [A] Added ReactiveVariableControllerWithUseCase

---
+++ Runtime/Core/Installers/ReactiveVariableControllerWithUseCaseInstaller.cs
@@ -0,0 +1,22 @@
+﻿using DependencyInjector.Core;
+using MVVM.Core.InterfaceAdapters;
+
+namespace MVVM.Core.Installers
+{
+    public abstract class ReactiveVariableControllerWithUseCaseInstaller<TType, TUseCase> : ReactiveVariableControllerInstaller<TType>
+    {
+        [Inject] private TUseCase _useCase;
+        
+        protected override void InstallServiceInContainer(IDIContainer diContainer, IController serviceInstance)
+        {
+            diContainer.RegisterAsMultiple(serviceInstance);
+        }
+
+        protected override IController GetController(IReactiveVariable<TType> reactiveVariable)
+        {
+            return GetReactiveController(reactiveVariable, _useCase);
+        }
+
+        protected abstract IController GetReactiveController(IReactiveVariable<TType> reactiveVariable, TUseCase useCase);
+    }
+}
---
+++ Runtime/Core/InterfaceAdapters/EventViewModel/Controllers/ReactiveVariableControllerWithUseCase.cs
@@ -0,0 +1,18 @@
+﻿namespace MVVM.Core.InterfaceAdapters
+{
+    public abstract class ReactiveVariableControllerWithUseCase<TType, TUseCase> : ReactiveVariableController<TType>
+    {
+        private readonly TUseCase _useCase;
+
+        protected ReactiveVariableControllerWithUseCase(IReactiveVariable<TType> reactiveVariable) : base(reactiveVariable)
+        {
+        }
+
+        protected override void ExecuteWithValue(TType value)
+        {
+            ExecuteUseCase(_useCase, value);
+        }
+
+        protected abstract void ExecuteUseCase(TUseCase useCase, TType value);
+    }
+}
