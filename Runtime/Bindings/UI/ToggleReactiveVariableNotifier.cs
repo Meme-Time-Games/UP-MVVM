18e751cf3d7fd691c061d3dadb598ab4ca2ac6af
David Toranzo <38369752+david-toranzo@users.noreply.github.com>
2025-02-28 19:53:37 -0300

    [A] Added toggle reactive variable notifier

---
+++ Runtime/Bindings/UI/ToggleReactiveVariableNotifier.cs
@@ -0,0 +1,27 @@
+﻿using MVVM.Core;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace View
+{
+    public class ToggleReactiveVariableNotifier : MonoBehaviour
+    {
+        [SerializeField] private Toggle _toggleToNotify;
+        [SerializeField] private ReactiveVariableSO<bool> _toggleToNotifyReactiveVariableSO;
+
+        private void Awake()
+        {
+            _toggleToNotify.onValueChanged.AddListener(HandleToggle);
+        }
+
+        private void HandleToggle(bool toggleValue)
+        {
+            _toggleToNotifyReactiveVariableSO.GetReactiveVariable().SetValueAndNotify(toggleValue);
+        }
+
+        private void OnDestroy()
+        {
+            _toggleToNotify.onValueChanged.RemoveListener(HandleToggle);
+        }
+    }
+}
