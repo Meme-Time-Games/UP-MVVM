f12048522ca0b22991bd0928c9e63e6395b850cc
FedeJIS <fede97cem@gmail.com>
2025-03-21 17:49:11 -0300

    [A] Added bool reactive variable injector initializer

---
+++ Runtime/DependencyInjector/Injectors/Installers/BoolReactiveVariableInjectorInitializer.cs
@@ -0,0 +1,28 @@
+﻿using DependencyInjector.Installers;
+using MVVM.Core;
+using UnityEngine;
+
+namespace MVVM
+{
+    public class BoolReactiveVariableInjectorInitializer : MonoBehaviour
+    {
+        [SerializeField] private BaseMonoInjector _baseMonoInjector;
+        [SerializeField] private ReactiveVariableSO<bool> _reactiveVariable;
+        [SerializeField] private bool _activateCondition;
+
+        private void Awake()
+        {
+            Inject();
+        }
+
+        private void Inject()
+        {
+            if (_activateCondition != _reactiveVariable.GetReactiveVariable().Value)
+            {
+                return;
+            }
+            
+            _baseMonoInjector.InjectAll();
+        }
+    }
+}
