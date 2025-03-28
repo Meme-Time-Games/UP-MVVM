a7d75bb779bf472297c4a0e8073dc19b85f47eb0
FedeJIS <fede97cem@gmail.com>
2025-03-28 13:20:47 -0300

    [A] Added raise event view models on dispose

---
+++ Runtime/Core/Installers/RaiseDisposeEventViewModelInstaller.cs
@@ -0,0 +1,16 @@
+﻿using DependencyInjector.Installers;
+using MVVM.Core;
+using MVVM.Core.InterfaceAdapters;
+using UnityEngine;
+
+namespace Core.Installers
+{
+    public class DisposeEventViewModelControllerInstaller : SingleMonoInstaller<RaiseDisposeEventViewModel>
+    {
+        [SerializeField] private EventViewModelSO _eventViewModelSO;
+        protected override RaiseDisposeEventViewModel GetData()
+        {
+            return new RaiseDisposeEventViewModel(_eventViewModelSO.GetEventViewModel());
+        }
+    }
+}
---
+++ Runtime/Core/InterfaceAdapters/EventViewModel/Controllers/RaiseDisposeEventViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MVVM.Core.InterfaceAdapters
+{
+    public class RaiseDisposeEventViewModel : IDisposable
+    {
+        private readonly IEventViewModel _eventViewModel;
+
+        public RaiseDisposeEventViewModel(IEventViewModel eventViewModel)
+        {
+            _eventViewModel = eventViewModel;
+        }
+        public void Dispose()
+        {
+            _eventViewModel.RaiseEvent();
+        }
+    }
+}
