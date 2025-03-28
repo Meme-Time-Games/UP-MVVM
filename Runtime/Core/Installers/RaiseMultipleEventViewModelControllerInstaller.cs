9f5d1e2dbaf47ef9813b2c1c342c7dd4722aa49c
FedeJIS <fede97cem@gmail.com>
2025-03-28 11:03:58 -0300

    [A] Added raise multiple event view models from one event

---
+++ Runtime/Core/Installers/RaiseMultipleEventViewModelControllerInstaller.cs
@@ -0,0 +1,24 @@
+﻿using DependencyInjector.Installers;
+using UnityEngine;
+
+namespace MVVM.Core.InterfaceAdapters
+{
+    public class RaiseMultipleEventViewModelControllerInstaller : SingleMonoInstaller<IController>
+    {
+            [Header("References")] 
+            [SerializeField] private EventViewModelSO _subscribeToEventViewModelSO;
+            [SerializeField] private EventViewModelSO[] _raiseEventsViewModelSO;
+        
+            protected override IController GetData()
+            {
+                IEventViewModel[] eventsViewModel = new IEventViewModel[_raiseEventsViewModelSO.Length];
+                
+                for (int i = 0; i < _raiseEventsViewModelSO.Length; i++)
+                {
+                    eventsViewModel[i] = _raiseEventsViewModelSO[i].GetEventViewModel();
+                }
+                
+                return new RaiseMultipleEventViewModelController(_subscribeToEventViewModelSO.GetEventViewModel(), eventsViewModel);
+            }
+        }
+}
---
+++ Runtime/Core/InterfaceAdapters/EventViewModel/Controllers/RaiseMultipleEventViewModelController.cs
@@ -0,0 +1,20 @@
+﻿namespace MVVM.Core.InterfaceAdapters
+{
+    public class RaiseMultipleEventViewModelController : Controller
+    {
+        private readonly IEventViewModel[] _raiseEventViewModel;
+        
+        public RaiseMultipleEventViewModelController(IEventViewModel eventViewModel, IEventViewModel[] raiseEventViewModel) : base(eventViewModel)
+        {
+            _raiseEventViewModel = raiseEventViewModel;
+        }
+
+        public override void Execute()
+        {
+            foreach (IEventViewModel raiseEventViewModel in _raiseEventViewModel)
+            {
+                raiseEventViewModel.RaiseEvent();
+            }
+        }
+    }
+}
