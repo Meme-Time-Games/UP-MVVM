3fc6d5bfbf679a6e6cac76564fb8fe76ee92479c
David Toranzo <38369752+david-toranzo@users.noreply.github.com>
2025-05-09 17:17:17 -0300

    [A] Added ExecuteCommandUsingReactiveVariableController

---
+++ Runtime/Core/Installers/ExecuteCommandUsingReactiveVariableControllerInstaller.cs
@@ -0,0 +1,23 @@
+﻿using Commands.Core;
+using DependencyInjector.Core;
+using MVVM.Core.InterfaceAdapters;
+using UnityEngine;
+
+namespace MVVM.Core.Installers
+{
+    public class ExecuteCommandUsingReactiveVariableControllerInstaller<TUseCase> : ControllerInstaller
+    {
+        [SerializeField] private ReactiveVariableSO<TUseCase> _reactiveVariableSO;
+        [Inject] private ICommandWithType<TUseCase> _commandWithType;
+
+        protected override void InstallServiceInContainer(IDIContainer diContainer, IController serviceInstance)
+        {
+            diContainer.RegisterAsMultiple(serviceInstance);
+        }
+
+        protected override IController GetController(IEventViewModel eventBindingViewModel)
+        {
+            return new ExecuteCommandUsingReactiveVariableController<TUseCase>(eventBindingViewModel, _reactiveVariableSO.GetReactiveVariable(), _commandWithType);
+        }
+    }
+}
---
+++ Runtime/Core/InterfaceAdapters/EventViewModel/Controllers/ExecuteCommandUsingReactiveVariableController.cs
@@ -0,0 +1,24 @@
+﻿using Commands.Core;
+
+namespace MVVM.Core.InterfaceAdapters
+{
+    public class ExecuteCommandUsingReactiveVariableController<TType> : Controller
+    {
+        private readonly IReactiveVariable<TType> _reactiveVariable;
+        private readonly ICommandWithType<TType> _commandWithType;
+
+        public ExecuteCommandUsingReactiveVariableController(IEventViewModel eventViewModel, IReactiveVariable<TType> reactiveVariable, 
+            ICommandWithType<TType> commandWithType) : base(eventViewModel)
+        {
+            _reactiveVariable = reactiveVariable;
+            _commandWithType = commandWithType;
+        }
+
+        public override void Execute()
+        {
+            TType value = _reactiveVariable.Value;
+
+            _commandWithType.Execute(value);
+        }
+    }
+}
