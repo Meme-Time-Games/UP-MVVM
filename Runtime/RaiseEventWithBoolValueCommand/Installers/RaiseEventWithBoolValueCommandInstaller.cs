6f86d1edcd99a9629ceae2dd0eeced930b3d2c63
Lalo <laurencioberro@hotmail.com>
2025-05-21 12:51:31 -0300

    [A] Added RaiseEventWithBoolValueCommand

---
+++ Runtime/RaiseEventWithBoolValueCommand/Installers/RaiseEventWithBoolValueCommandInstaller.cs
@@ -0,0 +1,18 @@
+﻿using MVVM.Core;
+using UnityEngine;
+
+namespace Commands.Core
+{
+    public class RaiseEventWithBoolValueCommandInstaller : CommandWithTypeInstaller<bool>
+    {
+        [Header("References")] 
+        [SerializeField] private EventViewModelSO _onTrueEventViewModelSO;
+        [SerializeField] private EventViewModelSO _onFalseEventViewModelSO;
+        
+        protected override ICommandWithType<bool> GetData()
+        {
+            return new RaiseEventWithBoolValueCommand(_onTrueEventViewModelSO.GetEventViewModel(),
+                _onFalseEventViewModelSO.GetEventViewModel());
+        }
+    }
+}
