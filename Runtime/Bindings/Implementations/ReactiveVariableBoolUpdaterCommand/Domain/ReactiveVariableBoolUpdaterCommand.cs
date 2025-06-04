a2167c243db31f3110bdd4320d3cbcbb02730c05
David Toranzo <38369752+david-toranzo@users.noreply.github.com>
2025-06-03 22:01:13 -0300

    [A] Added ReactiveVariableBoolUpdaterCommand

---
+++ Runtime/Bindings/Implementations/ReactiveVariableBoolUpdaterCommand/Domain/ReactiveVariableBoolUpdaterCommand.cs
@@ -0,0 +1,21 @@
+﻿using Commands.Core;
+
+namespace MVVM.Core.InterfaceAdapters
+{
+    public class ReactiveVariableBoolUpdaterCommand : ICommand
+    {
+        private readonly IReactiveVariable<bool> _boolReactiveVariable;
+        private readonly bool _valueToSet;
+
+        public ReactiveVariableBoolUpdaterCommand(IReactiveVariable<bool> boolReactiveVariable, bool valueToSet)
+        {
+            _boolReactiveVariable = boolReactiveVariable;
+            _valueToSet = valueToSet;
+        }
+
+        public void Execute()
+        {
+            _boolReactiveVariable.SetValue(_valueToSet);
+        }
+    }
+}
---
+++ Runtime/Bindings/Implementations/ReactiveVariableBoolUpdaterCommand/Installers/ReactiveVariableBoolUpdaterCommandInstaller.cs
@@ -0,0 +1,17 @@
+﻿using Commands.Core;
+using DependencyInjector.Installers;
+using UnityEngine;
+
+namespace MVVM.Core.InterfaceAdapters
+{
+    public class ReactiveVariableBoolUpdaterCommandInstaller : SingleMonoInstaller<ICommand>
+    {
+        [SerializeField] private bool _valueToSet;
+        [SerializeField] private ReactiveVariableSO<bool> _boolReactiveVariableSo;
+
+        protected override ICommand GetData()
+        {
+            return new ReactiveVariableBoolUpdaterCommand(_boolReactiveVariableSo.GetReactiveVariable(), _valueToSet);
+        }
+    }
+}
