6da21d9ccfa4305dacb2d64e00ddc2e42e4e00b6
David Toranzo <38369752+david-toranzo@users.noreply.github.com>
2025-06-25 23:17:44 -0300

    [A] Added ExecuteCommandUsingReactiveVariableController

---
+++ Runtime/Bindings/Implementations/ExecuteCommandUsingReactiveVariableController/StringExecuteCommandUsingReactiveVariableControllerInstaller.cs
@@ -0,0 +1,9 @@
+using MVVM.Core.Installers;
+
+namespace MVVM.Core.InterfaceAdapters
+{
+    public class StringExecuteCommandUsingReactiveVariableControllerInstaller : ExecuteCommandUsingReactiveVariableControllerInstaller<string>
+    {
+
+    }
+}
