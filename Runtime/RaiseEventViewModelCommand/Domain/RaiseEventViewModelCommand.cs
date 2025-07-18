adfdfcfbec2089b15cfe06a0932e8f8572826a0f
Lalo <laurencioberro@hotmail.com>
2025-07-18 15:22:24 -0300

    [A] Added RaiseEventViewModelCommand
    Introduces the RaiseEventViewModelCommand class for executing event actions on a ViewModel and its corresponding installer for dependency injection. Also updates package version to 1.24.0.

---
+++ Runtime/RaiseEventViewModelCommand/Domain/RaiseEventViewModelCommand.cs
@@ -0,0 +1,19 @@
+﻿using MVVM.Core;
+
+namespace Commands.Core
+{
+    public class RaiseEventViewModelCommand : ICommand
+    {
+        private readonly IEventViewModel _eventViewModel;
+
+        public RaiseEventViewModelCommand(IEventViewModel eventViewModel)
+        {
+            _eventViewModel = eventViewModel;
+        }
+
+        public void Execute()
+        {
+            _eventViewModel.RaiseEvent();
+        }
+    }
+}
---
+++ Runtime/RaiseEventViewModelCommand/Installers/RaiseEventViewModelCommandInstaller.cs
@@ -0,0 +1,17 @@
+﻿using DependencyInjector.Installers;
+using MVVM.Core;
+using UnityEngine;
+
+namespace Commands.Core
+{
+    public class RaiseEventViewModelCommandInstaller : SingleMonoInstaller<ICommand>
+    {
+        [Header("References")] 
+        [SerializeField] private EventViewModelSO _eventViewModelSO;
+        
+        protected override ICommand GetData()
+        {
+            return new RaiseEventViewModelCommand(_eventViewModelSO.GetEventViewModel());
+        }
+    }
+}
