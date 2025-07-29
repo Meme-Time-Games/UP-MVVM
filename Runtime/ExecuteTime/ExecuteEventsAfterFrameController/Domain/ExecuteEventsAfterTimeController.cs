94631d01a4bc01d174bea362f53019a83f69d4e7
David Toranzo <38369752+david-toranzo@users.noreply.github.com>
2025-07-29 19:09:20 -0300

    [A] Added ExecuteEventsAfterTimeController

---
+++ Runtime/ExecuteTime/ExecuteEventsAfterFrameController/Domain/ExecuteEventsAfterTimeController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using TimerRaiser;
+
+namespace MVVM.Core.InterfaceAdapters
+{
+    public class ExecuteEventsAfterTimeController : Controller
+    {
+        private readonly ITimer _timer;
+        private readonly float _timeToWait;
+        private readonly List<IEventViewModel> _eventViewModels; 
+
+        private int _currentIndex;
+
+        public ExecuteEventsAfterTimeController(IEventViewModel eventViewModel, List<IEventViewModel> eventViewModels, ITimer timer, float timeToWait) : base(eventViewModel) 
+        {
+            _eventViewModels = eventViewModels;
+            _timer = timer;
+            _timeToWait = timeToWait;
+            _currentIndex = 0; 
+        }
+
+        public override void Execute()
+        {
+            if (_eventViewModels != null && _eventViewModels.Count > 0)
+            {
+                _currentIndex = 0;
+
+                _timer.OnTimerDone += ExecuteNextEvent; 
+                _timer.StartTimer(_timeToWait);
+            }
+        }
+
+        private void ExecuteNextEvent()
+        {
+            _timer.OnTimerDone -= ExecuteNextEvent;
+
+            if (_currentIndex < _eventViewModels.Count)
+            {
+                _eventViewModels[_currentIndex].RaiseEvent();
+
+                _currentIndex++;
+
+                if (_currentIndex < _eventViewModels.Count)
+                {
+                    _timer.OnTimerDone += ExecuteNextEvent;
+                    _timer.StartTimer(_timeToWait);
+                }
+            }
+            else
+            {
+                _currentIndex = 0;
+            }
+        }
+    }
+}
---
+++ Runtime/ExecuteTime/ExecuteEventsAfterFrameController/Installers/ExecuteEventsAfterTimeControllerInstaller.cs
@@ -0,0 +1,33 @@
+﻿using DependencyInjector.Core;
+using MVVM.Core.Installers;
+using System.Collections.Generic;
+using TimerRaiser;
+using UnityEngine;
+
+namespace MVVM.Core.InterfaceAdapters
+{
+    public class ExecuteEventsAfterTimeControllerInstaller : ControllerInstaller
+    {
+        [SerializeField] private float _timeToWait;
+        [SerializeField] private EventViewModelSO[] _eventViewModelsToExecute;
+
+        [Inject] private ITimer _timer;
+
+        protected override IController GetController(IEventViewModel eventBindingViewModel)
+        {
+            List<IEventViewModel> eventsToExecute = new List<IEventViewModel>();
+
+            for (int i = 0; i < _eventViewModelsToExecute.Length; i++)
+            {
+                eventsToExecute[i] = _eventViewModelsToExecute[i].GetEventViewModel();
+            }
+
+            return new ExecuteEventsAfterTimeController(eventBindingViewModel, eventsToExecute, _timer, _timeToWait);
+        }
+
+        protected override void InstallServiceInContainer(IDIContainer diContainer, IController serviceInstance)
+        {
+            diContainer.RegisterAsMultiple(serviceInstance);
+        }
+    }
+}
