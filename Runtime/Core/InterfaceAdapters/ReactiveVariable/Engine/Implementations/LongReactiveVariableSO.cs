06ee3889244c43c4163f3c1c76727603673aff41
David Toranzo <38369752+david-toranzo@users.noreply.github.com>
2025-08-14 14:32:09 -0300

    [A] Added LongReactiveVariableSO

---
+++ Runtime/Core/InterfaceAdapters/ReactiveVariable/Engine/Implementations/LongReactiveVariableSO.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace MVVM.Core
+{
+    [CreateAssetMenu(fileName = "LongReactiveVariableSO", menuName = "ScriptableObjects/ReactiveVariable/Long")]
+    public class LongReactiveVariableSO : ReactiveVariableSO<long>
+    {
+        
+    }
+}
