ceb7e885883bd932a3f189d7a3f66e73d0af1983
FedeJIS <fede97cem@gmail.com>
2025-09-09 17:38:02 -0300

    [A] Added Vector2 reactive variable

---
+++ Runtime/Core/InterfaceAdapters/ReactiveVariable/Engine/Implementations/Vector2ReactiveVariableSO.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace MVVM.Core
+{
+    [CreateAssetMenu(fileName = "Vector2ReactiveVariableSO", menuName = "ScriptableObjects/ReactiveVariable/Vector2")]
+    public class Vector2ReactiveVariableSO : ReactiveVariableSO<Vector2>
+    {
+        
+    }
+}
