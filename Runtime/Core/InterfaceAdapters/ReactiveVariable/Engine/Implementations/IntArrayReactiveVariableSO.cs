18f28b8f050687fc1c39309650dbd8124cacc218
David Toranzo <38369752+david-toranzo@users.noreply.github.com>
2025-09-17 10:00:01 -0300

    [A] Added int array

---
+++ Runtime/Core/InterfaceAdapters/ReactiveVariable/Engine/Implementations/IntArrayReactiveVariableSO.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace MVVM.Core
+{
+    [CreateAssetMenu(fileName = "IntArray-ReactiveVariableSO", menuName = "ScriptableObjects/ReactiveVariable/Array-int")]
+    public class IntArrayReactiveVariableSO : ReactiveVariableSO<int[]>
+    {
+
+    }
+}
