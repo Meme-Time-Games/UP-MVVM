0445e7e941a19e0bc12b5ae272936a7b4824461e
Lalo <laurencioberro@hotmail.com>
2025-09-23 16:46:40 -0300

    [A] Added Array-Color Reactive Variable

---
+++ Runtime/Core/InterfaceAdapters/ReactiveVariable/Engine/Implementations/ArrayColorReactiveVariableSO.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace MVVM.Core
+{
+    [CreateAssetMenu(fileName = "ArrayColorReactiveVariableSO", menuName = "ScriptableObjects/ReactiveVariable/Array-Color")]
+    public class ArrayColorReactiveVariableSO : ReactiveVariableSO<Color[]>
+    {
+        
+    }
+}
