9751f854be720c146141bbb96ff82eea2d10b486
Lalo <laurencioberro@hotmail.com>
2025-09-24 17:50:32 -0300

    [A] Added PlayAnimationWithTriggerController

---
+++ Runtime/Bindings/Animation/PlayAnimationWithTriggerController/Installers/PlayAnimationWithTriggerControllerInstaller.cs
@@ -0,0 +1,27 @@
+﻿using DependencyInjector.Core;
+using MVVM.Core;
+using MVVM.Core.Installers;
+using MVVM.Core.InterfaceAdapters;
+using UnityEngine;
+
+namespace MVVM.Bindings
+{
+    public class PlayAnimationWithTriggerControllerInstaller : ControllerInstaller
+    {
+        [Header("References")]
+        private Animator _animator;
+
+        [Header("Config")]
+        private string _triggerName;
+        
+        protected override void InstallServiceInContainer(IDIContainer diContainer, IController serviceInstance)
+        {
+            diContainer.RegisterAsMultiple(serviceInstance);
+        }
+
+        protected override IController GetController(IEventViewModel eventBindingViewModel)
+        {
+            return new PlayAnimationWithTriggerController(eventBindingViewModel, _animator, _triggerName);
+        }
+    }
+}
---
+++ Runtime/Bindings/Animation/PlayAnimationWithTriggerController/InterfaceAdapters/Controllers/PlayAnimationWithTriggerController.cs
@@ -0,0 +1,23 @@
+﻿using MVVM.Core;
+using MVVM.Core.InterfaceAdapters;
+using UnityEngine;
+
+namespace MVVM.Bindings
+{
+    public class PlayAnimationWithTriggerController : Controller
+    {
+        private readonly Animator _animator;
+        private readonly string _triggerName;
+        
+        public PlayAnimationWithTriggerController(IEventViewModel eventViewModel, Animator animator, string triggerName) : base(eventViewModel)
+        {
+            _animator = animator;
+            _triggerName = triggerName;
+        }
+
+        public override void Execute()
+        {
+            _animator.SetTrigger(_triggerName);
+        }
+    }
+}
