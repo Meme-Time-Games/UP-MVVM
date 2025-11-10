21b950f9b10fa3d7c8b7fa3ed449480503050fb1
Fricodev <fede97cem@gmail.com>
2025-11-10 11:11:47 -0300

    [A] Added new rvs

---
+++ Runtime/Core/InterfaceAdapters/ReactiveVariable/Engine/Implementations/BoolArrayReactiveVariableSO.cs
@@ -0,0 +1,6 @@
+using MVVM.Core;
+using UnityEngine;
+[CreateAssetMenu(fileName = "BoolArray-ReactiveVariableSO", menuName = "ScriptableObjects/MVVM/ReactiveVariables/Array-Bool")]
+public class BoolArrayReactiveVariableSO : ReactiveVariableSO<bool[]>
+{
+}
---
+++ Runtime/Core/InterfaceAdapters/ReactiveVariable/Engine/Implementations/StringArrayReactiveVariableSO.cs
@@ -0,0 +1,7 @@
+﻿using MVVM.Core;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "StringArray-ReactiveVariableSO", menuName = "ScriptableObjects/MVVM/ReactiveVariables/Array-String")]
+public class StringArrayReactiveVariableSO : ReactiveVariableSO<string[]>
+{
+}
