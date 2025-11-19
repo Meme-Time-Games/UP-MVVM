5dab6e97c3d8a05b916f1e6d9653fdc73326fdbc
David Toranzo <38369752+david-toranzo@users.noreply.github.com>
2025-11-19 09:20:48 -0300

    [A] Added TransformRaiseEventViewModelReactiveVariable

---
+++ Runtime/Bindings/Implementations/RaiseEventReactiveVariableImplementations/TransformRaiseEventViewModelReactiveVariableControllerInstaller.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace MVVM.Core.InterfaceAdapters
+{
+    public class TransformRaiseEventViewModelReactiveVariableControllerInstaller : RaiseEventViewModelReactiveVariableControllerInstaller<Transform>
+    {
+
+    }
+}
