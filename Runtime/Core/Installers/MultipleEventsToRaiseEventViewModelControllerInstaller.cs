d6070ca32494b9cc14056b149d7d171868946be0
Fricodev <fede97cem@gmail.com>
2025-11-25 13:41:09 -0300

    [A] Added multiple events to raise mvvm controller

---
+++ Runtime/Core/Installers/MultipleEventsToRaiseEventViewModelControllerInstaller.cs
@@ -0,0 +1,25 @@
+﻿using DependencyInjector.Installers;
+using MVVM.Core;
+using MVVM.Core.InterfaceAdapters;
+using UnityEngine;
+
+namespace Core.Installers
+{
+    public class MultipleEventsToRaiseEventViewModelControllerInstaller : MultipleMonoInstaller<IController>
+    {
+        [Header("References")] 
+        [SerializeField] private EventViewModelSO _toRaiseEventViewModelSO;
+        [SerializeField] private EventViewModelSO[] _toListenEventViewModelSO;
+        protected override IController GetData()
+        {
+            IEventViewModel[] toListenEventViewModels = new IEventViewModel[_toListenEventViewModelSO.Length];
+
+            for (int i = 0; i < toListenEventViewModels.Length; i++)
+            {
+                toListenEventViewModels[i] = _toListenEventViewModelSO[i].GetEventViewModel();
+            }
+            
+            return new MultipleEventsToRaiseEventViewModelController(_toRaiseEventViewModelSO.GetEventViewModel(), toListenEventViewModels);
+        }
+    }
+}
---
+++ Runtime/Core/InterfaceAdapters/EventViewModel/Controllers/MultipleEventsToRaiseEventViewModelController.cs
@@ -0,0 +1,32 @@
+﻿namespace MVVM.Core.InterfaceAdapters
+{
+    public class MultipleEventsToRaiseEventViewModelController : IController
+    {
+        private readonly IEventViewModel _toRaiseEventViewModel;
+        private readonly IEventViewModel[] _toListenEventViewModels;
+
+        public MultipleEventsToRaiseEventViewModelController(IEventViewModel toRaiseEventViewModel, IEventViewModel[] toListenEventViewModels)
+        {
+            _toRaiseEventViewModel = toRaiseEventViewModel;
+            _toListenEventViewModels = toListenEventViewModels;
+            
+            foreach (IEventViewModel eventViewModel in _toListenEventViewModels)
+            {
+                eventViewModel.OnEventRaised += Execute;
+            }
+        }
+
+        public void Dispose()
+        {
+            foreach (IEventViewModel eventViewModel in _toListenEventViewModels)
+            {
+                eventViewModel.OnEventRaised -= Execute;
+            }
+        }
+
+        public void Execute()
+        {
+            _toRaiseEventViewModel.RaiseEvent();
+        }
+    }
+}
