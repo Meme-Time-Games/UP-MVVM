a58b12e732a90559e1358bfba7a8f08c958752fe
David Toranzo <38369752+david-toranzo@users.noreply.github.com>
2025-12-15 13:18:31 -0300

    [U] Updated folders and added RaiseBoolReactiveSwitchValueCommand

---
+++ Runtime/Bindings/ReactiveBool/RaiseBoolReactiveSwitchValueCommand/Domain/RaiseBoolReactiveSwitchValueCommand.cs
@@ -0,0 +1,24 @@
+﻿using Commands.Core;
+
+namespace MVVM.Core.InterfaceAdapters
+{
+    public class RaiseBoolReactiveSwitchValueCommand : ICommand
+    {
+        private readonly IReactiveVariable<bool> _boolReactiveVariable;
+
+        private bool _currentValue;
+
+        public RaiseBoolReactiveSwitchValueCommand(IReactiveVariable<bool> boolReactiveVariable)
+        {
+            _boolReactiveVariable = boolReactiveVariable;
+
+            _currentValue = boolReactiveVariable.Value;
+        }
+
+        public void Execute()
+        {
+            _currentValue = !_currentValue;
+            _boolReactiveVariable.SetValue(_currentValue);
+        }
+    }
+}
---
+++ Runtime/Bindings/ReactiveBool/RaiseBoolReactiveSwitchValueCommand/Installers/RaiseBoolReactiveSwitchValueCommandInstaller.cs
@@ -0,0 +1,16 @@
+﻿using Commands.Core;
+using DependencyInjector.Installers;
+using UnityEngine;
+
+namespace MVVM.Core.InterfaceAdapters
+{
+    public class RaiseBoolReactiveSwitchValueCommandInstaller : SingleMonoInstaller<ICommand>
+    {
+        [SerializeField] private ReactiveVariableSO<bool> _boolReactiveVariableSo;
+
+        protected override ICommand GetData()
+        {
+            return new RaiseBoolReactiveSwitchValueCommand(_boolReactiveVariableSo.GetReactiveVariable());
+        }
+    }
+}
