17357a1e26fcf88cf817f0f64b896cf8b8f7c580
Fricodev <fede97cem@gmail.com>
2025-12-16 16:33:04 -0300

    [A] Added multiple game object activator controller

---
+++ Runtime/Bindings/GameObjectActivator/Installers/MultipleGameObjectActivatorControllerInstaller.cs
@@ -0,0 +1,27 @@
+﻿using DependencyInjector.Core;
+using MVVM.Core;
+using MVVM.Core.Installers;
+using MVVM.Core.InterfaceAdapters;
+using UnityEngine;
+
+namespace View.GameObjectActivator
+{
+    public class MultipleGameObjectActivatorControllerInstaller : ControllerInstaller
+    {
+        [Header("Config")] 
+        [SerializeField] private bool _isActive;
+
+        [Header("References")] 
+        [SerializeField] private GameObject[] _gameObjectsToActive;
+        
+        protected override void InstallServiceInContainer(IDIContainer diContainer, IController serviceInstance)
+        {
+            diContainer.RegisterAsMultiple(serviceInstance);
+        }
+
+        protected override IController GetController(IEventViewModel eventBindingViewModel)
+        {
+            return new MultipleGameObjectActivatorController(eventBindingViewModel, _isActive, _gameObjectsToActive);
+        }
+    }
+}
---
+++ Runtime/Bindings/GameObjectActivator/InterfaceAdapters/Controllers/MultipleGameObjectActivatorController.cs
@@ -0,0 +1,27 @@
+﻿using MVVM.Core;
+using MVVM.Core.InterfaceAdapters;
+using UnityEngine;
+
+namespace View.GameObjectActivator
+{
+    public class MultipleGameObjectActivatorController : Controller
+    {
+        private readonly bool _isActive;
+        private readonly GameObject[] _gameObjects;
+        
+        public MultipleGameObjectActivatorController(IEventViewModel eventViewModel, bool isActive, GameObject[] gameObjects) : base(eventViewModel)
+        {
+            _isActive = isActive;
+            _gameObjects = gameObjects;
+        }
+
+        public override void Execute()
+        {
+            foreach (GameObject gameObject in _gameObjects)
+            {
+                gameObject.SetActive(_isActive);
+            }
+           
+        }
+    }
+}
