991a4c37a4d36ddcafb856271ba0e7b2ed15274d
David Toranzo <38369752+david-toranzo@users.noreply.github.com>
2026-02-04 20:16:41 -0300

    [A] Added reset trigger

---
+++ Runtime/Bindings/Animation/ResetTriggerAnimationController/Installers/ResetTriggerAnimationControllerInstaller.cs
@@ -0,0 +1,27 @@
+﻿using DependencyInjector.Core;
+using MVVM.Core;
+using MVVM.Core.Installers;
+using MVVM.Core.InterfaceAdapters;
+using UnityEngine;
+
+namespace MVVM.Bindings
+{
+    public class ResetTriggerAnimationControllerInstaller : ControllerInstaller
+    {
+        [Header("References")]
+        [SerializeField] private Animator _animator;
+
+        [Header("Config")]
+        [SerializeField] private string _triggerName;
+
+        protected override void InstallServiceInContainer(IDIContainer diContainer, IController serviceInstance)
+        {
+            diContainer.RegisterAsMultiple(serviceInstance);
+        }
+
+        protected override IController GetController(IEventViewModel eventBindingViewModel)
+        {
+            return new ResetTriggerAnimationController(eventBindingViewModel, _animator, _triggerName);
+        }
+    }
+}
---
+++ Runtime/Bindings/Animation/ResetTriggerAnimationController/InterfaceAdapters/Controllers/ResetTriggerAnimationController.cs
@@ -0,0 +1,23 @@
+﻿using MVVM.Core;
+using MVVM.Core.InterfaceAdapters;
+using UnityEngine;
+
+namespace MVVM.Bindings
+{
+    public class ResetTriggerAnimationController : Controller
+    {
+        private readonly Animator _animator;
+        private readonly string _triggerName;
+
+        public ResetTriggerAnimationController(IEventViewModel eventViewModel, Animator animator, string triggerName) : base(eventViewModel)
+        {
+            _animator = animator;
+            _triggerName = triggerName;
+        }
+
+        public override void Execute()
+        {
+            _animator.ResetTrigger(_triggerName);
+        }
+    }
+}
