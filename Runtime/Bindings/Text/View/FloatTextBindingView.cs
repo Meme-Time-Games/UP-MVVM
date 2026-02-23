875d34b18a26a8b0d32c3b20a5f7b76339035d58
Fricodev <fede97cem@gmail.com>
2026-02-23 13:13:10 -0300

    [A] Added float text binding view

---
+++ Runtime/Bindings/Text/View/FloatTextBindingView.cs
@@ -0,0 +1,6 @@
+﻿namespace MVVM.Bindings
+{
+    public class FloatTextBindingView : GenericTextBindingView<float>
+    {
+    }
+}
--- Runtime/Bindings/Text/View/IntTextBindingView.cs
+++ Runtime/Bindings/Text/View/IntTextBindingView.cs
@@ -1,5 +1,3 @@
-using UnityEngine;
-
 namespace MVVM.Bindings
 {
     public class IntTextBindingView : GenericTextBindingView<int>
